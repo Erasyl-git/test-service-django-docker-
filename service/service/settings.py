c1bd3dbf74abbf4b4a42f321d82157f5da01a140
Erasyl <thetopor600@gamil.com>
2024-04-05 22:58:28 +0600

    query optimization

--- service/service/settings.py
+++ service/service/settings.py
@@ -1,17 +1,12 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-twx34wv2&@(d!vo09)=ro_v*5$bik$jiv817(usdv1iiyd8e88'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -65,7 +60,6 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -79,8 +73,6 @@ DATABASES = {
 
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,8 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -110,12 +100,23 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
+
--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,16 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Subscription
 from .serializers import SubscriptionSerializer
+from django.db.models import Prefetch
+from clients.models import Client
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                              'user__email')) 
+    )
     serializer_class = SubscriptionSerializer
 
     
